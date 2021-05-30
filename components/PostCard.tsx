582614bc7e24f2081f8a161767d4ef104d864e40
Dhruv Jain <dhruvjainpenny@gmail.com>
2021-05-30 10:38:22 +0530

    fix: mobile design

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -18,12 +18,12 @@ const PostCard: React.FC<Props> = ({
   index,
 }: Props) => {
   const postWrapperClassNames = clsx(
-    "flex mb-8 w-full px-4 postcard",
-    index === 0 ? "lg:w-full" : "lg:w-1/3 lg:flex-col"
+    "flex mb-4 lg:mb-8 px-4 w-full postcard flex-col",
+    index === 0 ? "lg:flex-row" : "lg:w-1/3 lg:flex-col"
   );
-  const imageWrapperClassNames = clsx("mr-8 w-1/2", index !== 0 && "w-full");
-  const imageClassNames = clsx("w-full", index === 0 ? "h-80" : "h-64");
-  const contentWrapperClassNames = clsx("w-1/2", index !== 0 && "w-full");
+  const imageWrapperClassNames = clsx("mr-8 w-full", index !== 0 && "w-full");
+  const imageClassNames = clsx("w-full thumbnail");
+  const contentWrapperClassNames = clsx("w-full", index !== 0 && "w-full");
   const dateClassNames = clsx(
     "dark:text-white-dark",
     index === 0 ? "mb-1 lg:mb-4" : "lg:mt-4 mb-1"
@@ -37,7 +37,7 @@ const PostCard: React.FC<Props> = ({
         </div>
         <div className={contentWrapperClassNames}>
           <p className={dateClassNames}>{created_at}</p>
-          <h2 className="text-4xl font-bold mb-2 lg:mb-4 text-primary-regular">
+          <h2 className="text-2xl lg:text-3xl font-bold mb-2 lg:mb-4 text-primary-regular">
             {title}
           </h2>
           <h3 className="text-xl lg:text-2xl dark:text-white-dark">
