4c6f44ce6c28d2bff0cc452146c60023c529584f
Dhruv Jain <dhruvjainpenny@gmail.com>
2021-05-31 10:16:14 +0530

    feat: add favicon and app icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/manifest.json' />
+        </Head>
         <body className='bg-white-regular dark:bg-black-light'>
           <Main />
           <NextScript />
